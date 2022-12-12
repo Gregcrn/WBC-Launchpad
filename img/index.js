fa6d83b214224ceb8b3620702fb2b54aeaad2ed6
Gregcrn <gcorin65@gmail.com>
2022-12-12 04:15:59 -0400

    add img

---
+++ img/index.js
@@ -0,0 +1,33 @@
+import wineBottleLogo from './logo-wine-bottle-club.png';
+import member from './member.jpg';
+import user1 from './user-1.png';
+import facebook from './facebook.svg';
+import twitter from './twitter.svg';
+import telegram from './telegram.svg';
+import vector from './VectorHIW.svg';
+import zgcmq from './zgcmq.svg';
+import hero from './hero.png';
+import domain1 from './domain1.jpeg';
+import domain2 from './domain2.jpeg';
+import domain3 from './domain3.jpeg';
+import loader from './loadergif.gif';
+import nft1 from './nft1.png';
+import service from './service-1.png';
+
+export default {
+    service,
+    wineBottleLogo,
+    domain1,
+    domain2,
+    domain3,
+    hero,
+    member,
+    user1,
+    facebook,
+    telegram,
+    twitter,
+    vector,
+    zgcmq,
+    loader,
+    nft1,
+};
